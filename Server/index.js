a35bbc118850008ba97f2af8a9f33ecd3536b672
JFelixOJ <72662534+JFelixOJ@users.noreply.github.com>
2021-09-24 14:42:22 +0200

    added server

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 }, () => {
+
+    console.log('server started')
+
+})
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', (data) => {
+
+
+        // console.log(buffer.toString('utf8')('data received \n %o', data));
+        const buf = Buffer.from(data, 'utf8');
+        buf.toString()
+        console.log(buf.toString());
+
+
+        
+        ws.send(buf.toString());
+    })
+
+})
+
+wss.on('listening', () =>
+{
+    console.log('listening on 8080')
+})
