f9108fbf0c0866b74178a945c8fa6ed3c9f63d4b
Ruih11 <72733709+Ruih11@users.noreply.github.com>
2021-09-30 11:23:39 +0200

    Arduino and Unity connection

--- Server/test927.js
+++ Server/test927.js
@@ -1,9 +1,17 @@
 const Serialport = require('serialport');
 const Readline = Serialport.parsers.Readline;
+const WebSocket = require('ws')
 
 const port = new Serialport('COM3',{
     baudRate:9600
 });
+
+  const wss = new WebSocket.Server({ port: 8080 })
+
+  wss.on('connection', ws => {
+    ws.on('message', message => {
+      console.log(`Received message => ${message}`)
+    })
 
 const parser = port.pipe(new Readline({delimiter:'\r\n'}));
 
@@ -14,8 +22,20 @@ parser.on('open',function(){
 parser.on('data',function(data){
     let temp = parseInt(data , 10) + " ℃"
     console.log(temp);
+    ws.send(temp);
 });
 
 parser.on('error',function(err){
     console.log(err);
+    ws.send(err);
 });
+
+
+
+
+wss.on('listening', () =>
+{
+    console.log('listening on 8080')
+})
+
+  })
