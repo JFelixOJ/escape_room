a1eb2e955593fa9b9e3aa6da42ddab212523ef20
JFelixOJ <72662534+JFelixOJ@users.noreply.github.com>
2021-10-02 10:18:10 +0200

    New added packages

--- Assets/WS_Client.cs
+++ Assets/WS_Client.cs
@@ -3,16 +3,41 @@ using WebSocketSharp;
 public class WS_Client : MonoBehaviour
 {
     WebSocket ws;
+    public GameObject cube;
+  
     private void Start()
     {
-        ws = new WebSocket("ws://localhost:8080");
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        var BallRenderer = cube.GetComponent<Renderer>();
+     //   BallRenderer.material.SetColor("_Color", Color.green);
+        cube.transform.position = new Vector3((float)6.27, (float)1.64, (float)5.7);
+
+        ws = new WebSocket("ws://192.168.43.76:8080");
         ws.Connect();
         ws.OnMessage += (sender, e) =>
         {
             //string json = JsonUtility.ToJson(e);
             // string json = JsonUtility.ToJson(e, true);
+            ws.Send((e.Data));
+            Debug.Log("Tsdshe temperature is " + e.Data);
 
-            Debug.Log("The temperature is " + e.Data);
+            // var temp = int.Parse(e.Data);
+
+
+            // attempt to parse the value using the TryParse functionality of the integer type
+           int i = System.Convert.ToInt32(e.Data);
+
+            Debug.Log("Look here: " + i);//9001
+
+            Debug.Log("testing");
+            if (i == i)
+            {
+                var BallRenderer = cube.GetComponent<Renderer>();
+                Debug.Log("Did we get gere? ");
+                BallRenderer.material.SetColor("_Color", Color.green);
+                Debug.Log("Did we get gere? ");
+            }
+
 
 
         };
@@ -32,6 +57,10 @@ public class WS_Client : MonoBehaviour
         {
             ws.Send("2");
         }
+        /*if (Input.GetButton("Button.One"));
+        {
+            ws.Send("4");
+        }*/ //Doesnt work
     }
 
 }
